8220e2d1850008cd75badd365bc4a9948f167a1d
Thenula09 <thenulahansaja12@gmail.com>
2025-09-02 23:43:08 +0530

    update
    update

--- vite-react-app/src/frontend/components/Navbar/Navbar.jsx
+++ vite-react-app/src/frontend/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
         <span>My App</span>
       </div>
       <ul className="navbar-links">
-        <li><a href="/">Home</a></li>
+        <li><a href="/">Homce</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
