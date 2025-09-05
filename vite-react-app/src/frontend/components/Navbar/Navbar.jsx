06f145629f9d5f24cff897c8685ffa599b0675b2
Thenula09 <thenulahansaja12@gmail.com>
2025-09-05 11:44:31 +0530

    new
    add

--- vite-react-app/src/frontend/components/Navbar/Navbar.jsx
+++ vite-react-app/src/frontend/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
       <ul className="navbar-links">
         <li><a href="/">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#blog">Blog</a></li>
+        <li><a href="#blog">Bloge</a></li>
         <li><a href="#contact">Contact</a></li>
         <li><a href="#signin" className="signbtn">Sign in</a></li>
       </ul>
